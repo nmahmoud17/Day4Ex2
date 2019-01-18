ec7cc81dcefa74c8a5019907562bdbe02fec93f6
Nada Mahmoud <nadamahmoud@Nadas-MBP.T-mobile.com>
2019-01-17 21:55:43 -0500

    Testing Array Fxn

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,8 +3,15 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
+        int max = Integer.MAX_VALUE;
+        int i;
+        int j[] = new int []{1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20};
+        for (i = 2050; i<max; i++) {
+                if(i % j == 0 ) {
+                    System.out.println(j);
+            }
 
-        String Testingkdkd;
 
+        }
     }
 }
